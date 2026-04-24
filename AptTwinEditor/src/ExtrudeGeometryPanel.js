dcd41a283f9c4aa7aa98332ac74f044a6ef199c4
Hyunseok Min <hsmin@dahan.co.kr>
2026-04-24 11:44:41 +0900

    Extrude Geometry changed

--- AptTwinEditor/src/ExtrudeGeometryPanel.js
+++ AptTwinEditor/src/ExtrudeGeometryPanel.js
@@ -18,138 +18,54 @@ export class ExtrudeGeometryPanel {
 
         this.container = new UIDiv();
 
-        // width, height, depth
-        this.boxDimensionRow1 = new UIRow();
-        this.boxDimensionRow2 = new UIRow();
-        this.boxDimensionRow3 = new UIRow();
-        // this.boxDimensionRow.add( new UIText( 'Dimension' ).setClass( 'Label' ) );
+        // height
+        this.extrudeDimensionRow2 = new UIRow();
 
-        var boxwidthtitle = new UIText( 'width' ).setClass( 'Label' ).setWidth( '120px' );
-        this.boxWidth = new UIInput().setWidth( '100px' ).setFontSize( '12px' );
-        var boxheighttitle = new UIText( 'height' ).setClass( 'Label' ).setWidth( '120px' );
-        this.boxHeight = new UIInput().setWidth( '100px' ).setFontSize( '12px' );
-        var boxdepthtitle = new UIText( 'depth' ).setClass( 'Label' ).setWidth( '120px' );
-        this.boxDepth = new UIInput().setWidth( '100px' ).setFontSize( '12px' );
+        var extrudeheighttitle = new UIText( 'height' ).setClass( 'Label' ).setWidth( '120px' );
+        this.extrudeHeight = new UIInput().setWidth( '100px' ).setFontSize( '12px' );
 
-        var boxDimensionChangedFunc = this.boxDimensionChanged.bind(this);
-        this.boxWidth.onChange( boxDimensionChangedFunc );
-        this.boxHeight.onChange( boxDimensionChangedFunc );
-        this.boxDepth.onChange( boxDimensionChangedFunc );
+        var extrudeDimensionChangedFunc = this.extrudeDimensionChanged.bind(this);
+        this.extrudeHeight.onChange( extrudeDimensionChangedFunc );
 
-        this.boxDimensionRow1.add(boxwidthtitle, this.boxWidth);
-        this.boxDimensionRow2.add(boxheighttitle, this.boxHeight);
-        this.boxDimensionRow3.add(boxdepthtitle, this.boxDepth);
+        this.extrudeDimensionRow2.add(extrudeheighttitle, this.extrudeHeight);
 
-        this.container.add( this.boxDimensionRow1, this.boxDimensionRow2, this.boxDimensionRow3 );
+        this.container.add( this.extrudeDimensionRow2 );
 
         // type
-        this.boxMaterialClassRow = new UIRow();
+        this.extrudeMaterialClassRow = new UIRow();
 
-        this.boxMaterialClassRow.add( new UIText( 'Type' ).setClass( 'Label' ) );
+        this.extrudeMaterialClassRow.add( new UIText( 'Type' ).setClass( 'Label' ) );
         this.materialClass = new UISelect().setWidth( '150px' ).setFontSize( '12px' );
         
         this.materialClass.setOptions( meshMaterialOptions );
         var materialTypeChangedFunc = this.materialTypeChanged.bind(this);
         this.materialClass.onChange( materialTypeChangedFunc );
 
-        this.boxMaterialClassRow.add( this.materialClass );
+        this.extrudeMaterialClassRow.add( this.materialClass );
 
-        this.container.add( this.boxMaterialClassRow );
+        this.container.add( this.extrudeMaterialClassRow );
 
         // map
-        const leftRow = new UIRow();
-        const rightRow = new UIRow();
-        const topRow = new UIRow();
-        const bottomRow = new UIRow();
-        const insideRow = new UIRow();
-        const outsideRow = new UIRow();
+        this.extrudeMapRow = new UIRow();
+        this.extrudeMapRow.add( new UIText( 'Map' ).setClass( 'Label' ) );
 
-        var onMapChangedFunc = this.boxMapChanged.bind(this);
-        var onOpacityChangedFunc = this.opacityChanged.bind(this);
+        var onMapChange = this.extrudeMapChanged.bind(this);
+        this.extrudeMap = new UITexture2( showTextureImages );
 
-        var lefttitle = new UIText( 'Left Map' ).setClass( 'Label' ).setWidth( '120px' );
-        this.leftMap = new UITexture2( showTextureImages ).onChange( onMapChangedFunc );
-        var leftopacityTitle = new UIText( 'opacity' ).setClass( 'Label' ).setWidth( '50px' );
-        this.leftOpacity = new UIBoolean().setWidth( '100px' ).setFontSize( '12px' );
-        var removeLeftButton = new UIButton( 'Remove' ).setWidth( '80px' ).setMarginLeft( '10px' ).setMarginRight( '10px' );
-        this.leftOpacity.onChange( onOpacityChangedFunc );
-        removeLeftButton.onClick( () => { this.boxMapRemoved(0); });
+        var removeMapButton = new UIButton( 'Remove' ).setWidth( '80px' ).setMarginLeft( '10px' ).setMarginRight( '10px' );
+        removeMapButton.onClick( () => { this.mapRemoved(); });
 
-        var righttitle = new UIText( 'Right Map' ).setClass( 'Label' ).setWidth( '120px' );
-        this.rightMap = new UITexture2( showTextureImages ).onChange( onMapChangedFunc );
-        var rightopacityTitle = new UIText( 'opacity' ).setClass( 'Label' ).setWidth( '50px' );
-        this.rightOpacity = new UIBoolean().setWidth( '100px' ).setFontSize( '12px' );
-        var removeRightButton = new UIButton( 'Remove' ).setWidth( '80px' ).setMarginLeft( '10px' ).setMarginRight( '10px' );
-        this.rightOpacity.onChange( onOpacityChangedFunc );
-        removeRightButton.onClick( () => { this.boxMapRemoved(1); });
+        this.extrudeMap.onChange( onMapChange );
+        this.extrudeMapRow.add( this.extrudeMap, removeMapButton );
 
-        var toptitle = new UIText( 'Top Map' ).setClass( 'Label' ).setWidth( '120px' );
-        this.topMap = new UITexture2( showTextureImages ).onChange( onMapChangedFunc );
-        var topopacityTitle = new UIText( 'opacity' ).setClass( 'Label' ).setWidth( '50px' );
-        this.topOpacity = new UIBoolean().setWidth( '100px' ).setFontSize( '12px' );
-        var removeTopButton = new UIButton( 'Remove' ).setWidth( '80px' ).setMarginLeft( '10px' ).setMarginRight( '10px' );
-        this.topOpacity.onChange( onOpacityChangedFunc );
-        removeTopButton.onClick( () => { this.boxMapRemoved(2); });
-
-        var bottomtitle = new UIText( 'Bottom Map' ).setClass( 'Label' ).setWidth( '120px' );
-        this.bottomMap = new UITexture2( showTextureImages ).onChange( onMapChangedFunc );
-        var bottomopacityTitle = new UIText( 'opacity' ).setClass( 'Label' ).setWidth( '50px' );
-        this.bottomOpacity = new UIBoolean().setWidth( '100px' ).setFontSize( '12px' );
-        var removeBottomButton = new UIButton( 'Remove' ).setWidth( '80px' ).setMarginLeft( '10px' ).setMarginRight( '10px' );
-        this.bottomOpacity.onChange( onOpacityChangedFunc );
-        removeBottomButton.onClick( () => { this.boxMapRemoved(3); });
-
-        var insidetitle = new UIText( 'Inside Map' ).setClass( 'Label' ).setWidth( '120px' );
-        this.insideMap = new UITexture2( showTextureImages ).onChange( onMapChangedFunc );
-        var insideopacityTitle = new UIText( 'opacity' ).setClass( 'Label' ).setWidth( '50px' );
-        this.insideOpacity = new UIBoolean().setWidth( '100px' ).setFontSize( '12px' );
-        var removeInsideButton = new UIButton( 'Remove' ).setWidth( '80px' ).setMarginLeft( '10px' ).setMarginRight( '10px' );
-        this.insideOpacity.onChange( onOpacityChangedFunc );
-        removeInsideButton.onClick( () => { this.boxMapRemoved(4); });
-
-        var outsidetitle = new UIText( 'Outside Map' ).setClass( 'Label' ).setWidth( '120px' );
-        this.outsideMap = new UITexture2( showTextureImages ).onChange( onMapChangedFunc );
-        var outsideopacityTitle = new UIText( 'opacity' ).setClass( 'Label' ).setWidth( '50px' );
-        this.outsideOpacity = new UIBoolean().setWidth( '100px' ).setFontSize( '12px' );
-        var removeOutsideButton = new UIButton( 'Remove' ).setWidth( '80px' ).setMarginLeft( '10px' ).setMarginRight( '10px' );
-        this.outsideOpacity.onChange( onOpacityChangedFunc );
-        removeOutsideButton.onClick( () => { this.boxMapRemoved(5); });
-
-        this.boxMaps = [];
-        this.boxMaps.push( this.leftMap, this.rightMap, this.topMap, this.bottomMap, this.insideMap, this.outsideMap );
-        this.opacities = [];
-        this.opacities.push( this.leftOpacity, this.rightOpacity, this.topOpacity, this.bottomOpacity, this.insideOpacity, this.outsideOpacity );
-
-        leftRow.add(lefttitle, this.leftMap, leftopacityTitle, this.leftOpacity, removeLeftButton);
-        rightRow.add(righttitle, this.rightMap, rightopacityTitle, this.rightOpacity, removeRightButton);
-        topRow.add(toptitle, this.topMap, topopacityTitle, this.topOpacity, removeTopButton);
-        bottomRow.add(bottomtitle, this.bottomMap, bottomopacityTitle, this.bottomOpacity, removeBottomButton);
-        insideRow.add(insidetitle, this.insideMap, insideopacityTitle, this.insideOpacity, removeInsideButton);
-        outsideRow.add(outsidetitle, this.outsideMap, outsideopacityTitle, this.outsideOpacity, removeOutsideButton);
-
-        this.container.add( leftRow );
-        this.container.add( rightRow );
-        this.container.add( topRow );
-        this.container.add( bottomRow );
-        this.container.add( insideRow );
-        this.container.add( outsideRow );
+        this.container.add( this.extrudeMapRow );
 
     }
 
-    boxDimensionChanged() {
-        this.editor.execute( new SetGeometryCommand( this.editor, this.editor.selected, new THREE.BoxGeometry(
-            this.boxWidth.getValue(), this.boxHeight.getValue(), this.boxDepth.getValue()
-        ) ) );
-    }
-
-    opacityChanged() {
-        let currentObject = this.editor.selected;
-
-        const newOpacities = [];
-        for(let i=0; i<6; i++)
-            newOpacities.push( this.opacities[i].getValue() );
-
-        this.editor.execute( new SetMaterialOpaqueCommand( this.editor, currentObject, newOpacities ) );        
+    extrudeDimensionChanged() {
+        // this.editor.execute( new SetGeometryCommand( this.editor, this.editor.selected, new THREE.ExtrudeGeometry(
+            
+        // ) ) );
     }
 
     materialTypeChanged() {
@@ -179,38 +95,30 @@ export class ExtrudeGeometryPanel {
         }
     }
     
-    boxMapChanged( texture ) {
+    extrudeMapChanged( texture ) {
         let object = this.editor.selected;
-        let materials = object.material;
+        let material = object.material;
 
         if ( texture !== null ) {
             texture.colorSpace = THREE.SRGBColorSpace;
-            materials.needsUpdate = true;
+            material.needsUpdate = true;
         }
 
-        const newMaps = [];
-        for(let i=0; i<6; i++)
-            newMaps.push( this.boxMaps[i].getValue() );
+        const newMap = this.extrudeMap.getValue();
 
-        this.editor.execute( new SetMaterialMapCommand( this.editor, object, 'map', newMaps ) );
-        
+        this.editor.execute( new SetMaterialMapCommand( this.editor, object, 'map', newMap ) );
+
         this.updateUI();
     }
 
-    boxMapRemoved( index ) {
+    mapRemoved( index ) {
         let object = this.editor.selected;
 
-        const newMaps = [];
-        for(let i=0; i<6; i++) {
-            if(i == index)
-                newMaps.push(null);
-            else
-                newMaps.push( this.boxMaps[i].getValue() );
-        }
+        const newMap = null;
 
-        this.editor.execute( new SetMaterialMapCommand( this.editor, object, 'map', newMaps ) );
+        this.editor.execute( new SetMaterialMapCommand( this.editor, object, 'map', newMap ) );
 
-         this.updateUI();
+        this.updateUI();
     }
 
     updateUI() {
@@ -218,20 +126,13 @@ export class ExtrudeGeometryPanel {
 
         const parameters = object.geometry.parameters;
 
-        this.boxWidth.setValue( parameters.width );
-        this.boxHeight.setValue( parameters.height );
-        this.boxDepth.setValue( parameters.depth );
+        this.extrudeHeight.setValue( parameters.height );
 
-        const materials = object.material;
-        if(!Array.isArray(materials))
-            return;
+        const material = object.material;
 
-        this.materialClass.setValue( materials[0].type );
+        this.materialClass.setValue( material.type );
 
-        for(let i=0; i<6; i++) {
-            this.boxMaps[i].setValue( materials[i][ 'map' ] );
-            this.opacities[i].setValue( materials[i].transparent );
-        }
+        this.extrudeMap.setValue( material[ 'map' ] );
     }    
 }
 
